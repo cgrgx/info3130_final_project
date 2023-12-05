fe3f4f1a49b3c0535faee8e8f58c4909eb46654e
chiran gurung <grgchiran08@yahoo.com>
2023-12-04 19:30:31 -0500

    Set base path for Github pages diployment

--- vite.config.js
+++ vite.config.js
@@ -4,5 +4,5 @@ import react from "@vitejs/plugin-react";
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: "/INFO3130-Final-Project/",
+  base: "/info3130_final_project/",
 });
